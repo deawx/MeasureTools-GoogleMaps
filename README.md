1fd4e01233f61b46411f6737e0de1159a1c1fb99
Aonan Li <ali@tetrad.com>
2017-06-29 11:34:46 -0700

    update library

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # MeasureTools-GoogleMaps
 It is a JavaScript library implementing lasso tool and straight line drawing based on Google Maps JavaScript API. Users can draw free-hand area with lasso tool or straight line with measure tool, which return an array containing all coordinates.
+
+measureTool library usage:
+
+use it inside initMap() after the declaration of map object:
+
+measureTool.config({
+  map: map,                          //your map object
+  measure_button: "measure_button",  //id of your measure button
+  lasso_button: "lasso_button",      //id of your lasso button
+  polylineConfig:null,               //your Polyline specification, set null if you want default setting
+  polygonConfig:null                 //your Polygon specification, set null if you want default setting
+  onLineEnd: function(coords) {
+    console.log(coords);             //coords is the coorditates array when users finish drawing, set null to disable
+  },
+  onLineMove: function(coords) {
+    console.log(coords);             //coords is the coorditates array when users keep drawing, use this if you want real-time distance calculations, set null to disable
+  }
+});
+
+documentation about PolylineOptions: https://developers.google.com/maps/documentation/javascript/reference#PolylineOptions
+
+default lasso config:
+{
+  strokeColor: '#FF0000',
+  strokeOpacity: 0.3,
+  strokeWeight: 2,
+  fillColor: '#FF0000',
+  fillOpacity: 0.25
+}
--- src/measureTool.js
+++ src/measureTool.js
@@ -1,3 +1,34 @@
+/*
+measureTool library usage:
+
+use it inside initMap() after the declaration of map object:
+
+measureTool.config({
+  map: map,                          //your map object
+  measure_button: "measure_button",  //id of your measure button
+  lasso_button: "lasso_button",      //id of your lasso button
+  polylineConfig:null,               //your Polyline specification, set null if you want default setting
+  polygonConfig:null                 //your Polygon specification, set null if you want default setting
+  onLineEnd: function(coords) {
+    console.log(coords);             //coords is the coorditates array when users finish drawing, set null to disable
+  },
+  onLineMove: function(coords) {
+    console.log(coords);             //coords is the coorditates array when users keep drawing, use this if you want real-time distance calculations, set null to disable
+  }
+});
+
+documentation about PolylineOptions: https://developers.google.com/maps/documentation/javascript/reference#PolylineOptions
+
+default lasso config:
+{
+  strokeColor: '#FF0000',
+  strokeOpacity: 0.3,
+  strokeWeight: 2,
+  fillColor: '#FF0000',
+  fillOpacity: 0.25
+}
+
+*/
 (function () {
   var map;
   var startX = 0;
@@ -8,54 +39,68 @@
   var isTouched = false;
   var line;
   var lineCoords;
-  var lineSymbol = {
-    path: 'M 0,-1 0,1',
-    strokeOpacity: 1,
-    scale: 1
-  };
   var lasso;
   var lassoCoords = [];
   var lineActive = false;
   var lassoActive = false;
   var el = document.body;
+  var onLineEnd;
+  var onLineMove;
   el.addEventListener("touchstart", handleStart, false);
   el.addEventListener("touchend", handleEnd, false);
 
   var api = {
-    config: function (yourmap) {
-      map = yourmap;
+    config: function (config) {
+      map = config.map;
       line = new google.maps.Polyline({
+        map: map
+      });
+      if(config.polylineConfig){
+        line.setOptions(config.polylineConfig);
+      }
+      else{
+        line.setOptions({ //default line config
           geodesic: true,
           strokeOpacity: 0,
           icons: [{
-            icon: lineSymbol,
+            icon: {
+              path: 'M 0,-1 0,1',
+              strokeOpacity: 1,
+              scale: 1
+            },
             offset: '0',
             repeat: '5px'
-          }],
-          map: map
-        });
-        lasso = new google.maps.Polygon({
-          paths: lassoCoords,
+          }]
+        })
+      }
+      lasso = new google.maps.Polygon({
+        map: map
+      });
+      if(config.polygonConfig){
+        lasso.setOptions(config.polygonConfig);
+      }
+      else{
+        lasso.setOptions({ //default lasso config
           strokeColor: '#FF0000',
           strokeOpacity: 0.3,
           strokeWeight: 2,
           fillColor: '#FF0000',
-          fillOpacity: 0.25,
-          map: map
-        });
+          fillOpacity: 0.25
+        })
+      }
+      var measure_button = document.getElementById(config.measure_button);
+      measure_button.addEventListener("click", drawLine, false);
+      var lasso_button = document.getElementById(config.lasso_button);
+      lasso_button.addEventListener("click", drawLasso, false);
+      onLineEnd = config.onLineEnd;
+      onLineMove = config.onLineMove;
     },
-    listen: function listen() {
-        var measure_button = document.getElementById("measure_button");
-        measure_button.addEventListener("click", drawLine, false);
-        var lasso_button = document.getElementById("lasso_button");
-        lasso_button.addEventListener("click", drawLasso, false);
-    }
   }
 
   function handleMoveLine (event) {
     if(lineActive && !hasOrigin && isTouched){
       startX = endX = event.latLng.lat();
-      startY = endY = event.latLng.lng(); 
+      startY = endY = event.latLng.lng();
       console.log("Setting origin: " + startX + ", " + startY);
       hasOrigin = true;
     }
@@ -67,6 +112,8 @@
         {lat:endX, lng:endY}
       ];
       line.setPath(lineCoords);
+      if(onLineMove)
+      onLineMove(lineCoords);
       console.log("Update line at: " + startX + ", " + startY + ", " + endX + ", " + endY)
     }
   }
@@ -74,13 +121,10 @@
   function handleMoveLasso (event) {
     if(lassoActive && !hasOrigin && isTouched){
       lassoCoords.push({lat: event.latLng.lat(), lng: event.latLng.lng()});
-      lassoCoords.push({lat: event.latLng.lat(), lng: event.latLng.lng()});
       hasOrigin = true;
     }
     else if(lassoActive && hasOrigin){
-      var temp = lassoCoords.pop();
       lassoCoords.push({lat: event.latLng.lat(), lng: event.latLng.lng()});
-      lassoCoords.push(temp); //could be done in handleEnd() to save cost
       lasso.setPath(lassoCoords);
     }
   }
@@ -98,13 +142,16 @@
   function handleEnd (evt){
     if(lineActive){
       console.log("The final line: ");
-      console.log(lineCoords) // object passed to calculateDistance();
+      if(onLineEnd)
+      onLineEnd(lineCoords);
       clearAll();
     }
 
     if(lassoActive){
+      lassoCoords.push(lassoCoords[0])
       console.log("The final area: ");
-      console.log(lassoCoords) // object passed to generateReport();
+      if(onLineEnd)
+      onLineEnd(lineCoords);
       clearAll();
     }
   }
